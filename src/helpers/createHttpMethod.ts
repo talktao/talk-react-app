bc094e3ad21bb0a231d1fdc582114fe9f155d79a
tantao <t1416249906@163.com>
2023-05-11 13:15:21 +0800

    add: axios.put/delete

--- src/helpers/createHttpMethod.ts
+++ src/helpers/createHttpMethod.ts
@@ -2,12 +2,18 @@
 export default function createHttpMethod(axios) {
     const get = (url, data) => axios.get(url, {
         params: data
-    })
+    });
 
     const post = (url, data) => axios.post(url, data);
 
+    const put = (url, data) => axios.put(url, data);
+
+    const del = (url, data) => axios.delete(url, data);
+
     return {
         get,
-        post
-    }
+        post,
+        put,
+        del
+    };
 }

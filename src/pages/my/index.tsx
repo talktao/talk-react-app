9724b52b0e92e6776ac75162a7d49b54606b283d
tantao <t1416249906@163.com>
2023-05-11 13:22:38 +0800

    fix： my page

---
+++ src/pages/my/index.tsx
@@ -0,0 +1,11 @@
+
+import Hello from '@/components/hello';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import style from './index.module.less';
+
+export default function My() {
+
+    return <div className={style.my}>
+
+    </div>;
+}

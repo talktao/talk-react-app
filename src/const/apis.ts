e2be011e81a906b5e89444187c2196120af449f9
tantao <t1416249906@163.com>
2023-05-11 12:30:10 +0800

    add: home page api

--- src/const/apis.ts
+++ src/const/apis.ts
@@ -1,17 +1,18 @@
 import { RequestTuple } from "@/type/request";
 
 const ApiCollector: {
-    [propName: string]: RequestTuple
+    [propName: string]: RequestTuple;
 } = {
-    
+
+    getHome: {
+        method: 'get',
+        url: '/api/home'
+    },
+
     getCmsCatalogList: {
         method: 'get',
-        // url: '/qcloud-cos/shenshipin-1253925857/credential',
         url: '/api/login',
-        // url: '/catalog/getCatalogList',
     }
-
-
-}
+};
 
 export default ApiCollector;

c129db9e8bde38c0d46ee2bc432c875a8d325772
tantao <t1416249906@163.com>
2023-05-11 13:21:23 +0800

    add: 封装 useRequest 返回的数据

--- src/hooks/useMockRequest.ts
+++ src/hooks/useMockRequest.ts
@@ -1,23 +1,23 @@
-import { cmsAxios } from "@/helpers/axios";
+import { cmsAxios, mockAxios } from "@/helpers/axios";
 import RequestProps, { RequestTuple } from "@/type/request";
 import { useRequest } from "ahooks";
 import get from 'lodash/get';
 import useAxiosMethods from "./useAxiosMethods";
 
 
-export default function useCMSRequest<T>(request: RequestTuple, params: RequestProps<T>) {
+export default function useMockRequest<T>(request: RequestTuple, params: RequestProps<T>) {
     const { method, url } = request;
     const { data = {}, config = {} } = params;
-    const controller = useAxiosMethods(cmsAxios);
+    const controller = useAxiosMethods(mockAxios);
 
-    if (!controller[method]) throw new Error('当前请求方法仅支持get/post');
+    if (!controller[method]) throw new Error('当前请求方法仅支持get/post/put/delete');
 
     const http = () => controller[method](url, data);
     const alibabaHook = useRequest(http, config);
     const { data: res } = alibabaHook;
-    const returnData = get(res, 'data.returnData', []) as T;
+    const returnData = get(res, 'data.data', []) as T;
     return {
         ...alibabaHook,
         data: returnData
-    }
+    };
 }

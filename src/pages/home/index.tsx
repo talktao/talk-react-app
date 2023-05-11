fe08485de37d70a7a2af50be9f11c79d9432a23e
tantao <t1416249906@163.com>
2023-05-11 13:22:25 +0800

    fix：home page code change

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,71 +1,49 @@
 import ApiCollector from '@/const/apis';
 import render from '@/helpers/render';
-import useCMSRequest from '@/hooks/useCMSRequest';
+import useMockRequest from '@/hooks/useMockRequest';
 import { Link, Outlet } from 'react-router-dom';
 import './index.less';
 import { observer } from 'mobx-react-lite';
 import counterStore from '@/store/count';
-import { useToggle } from 'ahooks';
+import { BulletList } from 'react-content-loader';
 
-interface Article {
-    id: number
-    name: string
+interface List {
+    id: number;
+    content: string;
 }
 
-function App() {
-    const catalogHttpController = useCMSRequest<Article[]>(ApiCollector.getCmsCatalogList, {
-        data: {
-            tenantId: 'ssp',
-            appCode: '510'
-        },
-        config: {
-            refreshDeps: [counterStore.count],
-            // retryCount: 3
-        }
-    });
-    const { data, error, loading } = catalogHttpController;
-    const [state, { toggle, setLeft, setRight }] = useToggle();
+interface Data {
+    title: string;
+    subTitle: string;
+    list: List[];
+}
+
+function Home() {
+    const homeHttpController = useMockRequest<Data>(ApiCollector.getHome, {});
+    const { data, error, loading } = homeHttpController;
+
+    if (loading) return <BulletList />;
 
     return <div className="app">
-        <div onClick={counterStore.increment}>测试store {counterStore.count}</div>
-        <div onClick={catalogHttpController.run}>点击我重新发出请求</div>
-        <div>
-            {
-                loading && 'http is fetching...'
-            }
-            {
-                data.map(i => {
-                    return <div key={i.id}>{i.id} - {i.name}</div>
-                })
-            }
-        </div>
-        <div>
-            <p>Effects：{`${state}`}</p>
-            <p>
-                <button type="button" onClick={toggle}>
-                    Toggle
-                </button>
-                <button type="button" onClick={setLeft} style={{ margin: '0 8px' }}>
-                    Toggle False
-                </button>
-                <button type="button" onClick={setRight}>
-                    Toggle True
-                </button>
-            </p>
-        </div>
-        <Outlet />
-        <Link to="/login">login</Link>
-    </div>
+        <div className='title'>{data.title}</div>
+        <div className='subTitle'>{data.subTitle}</div>
+        {
+            data.list.map(item => (
+                <div className='item' key={item.id}>
+                    <span>{item.id} - {item.content}</span>
+                </div>
+            ))
+        }
+    </div>;
 }
 
-export default observer(App);
-
+export default observer(Home);
 
 
 if (process.env.NODE_ENV === 'development') {
 
 } else {
-    render(<App />);
+    render(<Home />);
 }
 
 

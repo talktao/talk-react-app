1539315978c6c7042ea7f31077b3beddcfff0637
tantao <t1416249906@163.com>
2023-05-11 13:23:04 +0800

    add：RequestTuple ‘put/delete’

--- src/type/request.ts
+++ src/type/request.ts
@@ -2,13 +2,13 @@ import { Options } from "ahooks/lib/useRequest/src/types";
 
 
 export interface RequestTuple {
-    method: 'get' | 'post',
+    method: 'get' | 'post' | 'put' | 'delete',
     url: string,
 }
 
 export default interface RequestProps<T> {
     data?: any,
     config?: Options<{
-        data: T
-    }, any>
+        data: T;
+    }, any>;
 }

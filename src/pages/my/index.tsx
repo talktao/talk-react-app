a135a7eaaf0bc7f9dc2c059a574446e15e2ec605
tantao <t1416249906@163.com>
2023-05-14 22:06:52 +0800

    feat：my conponent 使用 tabbar

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -2,10 +2,12 @@
 import Hello from '@/components/hello';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import style from './index.module.less';
+import { routesUrl } from '@/const/routes';
+import Tabbar from '@/components/tabbar';
 
 export default function My() {
 
     return <div className={style.my}>
-
+        <Tabbar path={routesUrl.my} />
     </div>;
 }

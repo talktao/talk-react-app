e3039b9bf8a1b7bd25e3a0d6ad9b1b7052a9f736
tantao <t1416249906@163.com>
2023-05-15 22:29:09 +0800

    fix：font Withdrawal

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import render from './helpers/render';
+import './global/style/reset.css';
 
 
 render(<App />);
@@ -14,9 +15,9 @@ reportWebVitals();
 
 if (process.env.NODE_ENV === 'development') {
     require('./mock/index'); // 开启mock
-    const VConsole = require('vconsole');
-    var vConsoler = new VConsole();
-    console.log('development')
+    // const VConsole = require('vconsole');
+    // var vConsoler = new VConsole();
+    console.log('development');
 }
 
 

39babd21cafece94c15e4d914954a4ab20a2d846
tantao <t1416249906@163.com>
2023-05-15 22:01:09 +0800

    feat：add list page

---
+++ src/pages/list/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import ApiCollector from '@/const/apis';
+import useListPages from '@/hooks/useListPages';
+import FetchTips from '@/components/fetchTips';
+import styles from './index.module.less';
+import Card from './card';
+
+const List: FC = () => {
+
+    const { list, ifDone } = useListPages(ApiCollector.getArticleList, {});
+
+    console.log(list, 'list');
+
+
+    return <div className={styles.list}>
+        {
+            list.map((li, index) => (
+                <div className={styles.item} key={index}>
+                    <Card li={li} />
+                </div>
+            ))
+        }
+        {list.length !== 0 && <FetchTips ifDone={ifDone} />}
+    </div>;
+};
+
+export default List;

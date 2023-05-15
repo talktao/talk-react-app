5100d4125b39853f014852adb9b210b56da14b23
tantao <t1416249906@163.com>
2023-05-15 22:27:54 +0800

    feat：my page

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,13 +1,29 @@
-
-import Hello from '@/components/hello';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import style from './index.module.less';
+import { FC } from 'react';
 import { routesUrl } from '@/const/routes';
 import Tabbar from '@/components/tabbar';
+import Logo from '@/images/logo.png';
+import styles from './index.module.less';
+import magicStore from '@/store/magic';
+import { observer } from 'mobx-react-lite';
 
-export default function My() {
+const My: FC = () => {
 
-    return <div className={style.my}>
+    const onMagic = () => {
+        magicStore.triggerMagic('真乖，如果对您有帮助请在github上点个星星哦~ ');
+    };
+
+    return <div className={styles.my}>
+        <img src={Logo} alt="" />
+        <span className={styles.title}>React H5 开发模板</span>
+        <span className={styles.github}>项目地址：
+            <a href="https://github.com/talktao/talk-react-app">https://github.com/talktao/talk-react-app</a>
+        </span>
+        <span className={styles.author}>项目作者：talktao</span>
+        <span className={styles.btn}>
+            {magicStore.magicText === '' ? <button onClick={onMagic}>点我有魔法</button> : magicStore.magicText}
+        </span>
         <Tabbar path={routesUrl.my} />
     </div>;
-}
+};
+
+export default observer(My);

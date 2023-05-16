a776223281c5aec0bd5a7691a04649406d74a835
tantao <t1416249906@163.com>
2023-05-16 21:01:26 +0800

    re：coding check

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -5,6 +5,7 @@ import Logo from '@/images/logo.png';
 import styles from './index.module.less';
 import magicStore from '@/store/magic';
 import { observer } from 'mobx-react-lite';
+import Layout from '@/components/layout';
 
 const My: FC = () => {
 
@@ -12,18 +13,22 @@ const My: FC = () => {
         magicStore.triggerMagic('真乖，如果对您有帮助请在github上点个星星哦~ ');
     };
 
-    return <div className={styles.my}>
-        <img src={Logo} alt="" />
-        <span className={styles.title}>React H5 开发模板</span>
-        <span className={styles.github}>项目地址：
-            <a href="https://github.com/talktao/talk-react-app">https://github.com/talktao/talk-react-app</a>
-        </span>
-        <span className={styles.author}>项目作者：talktao</span>
-        <span className={styles.btn}>
-            {magicStore.magicText === '' ? <button onClick={onMagic}>点我有魔法</button> : magicStore.magicText}
-        </span>
-        <Tabbar path={routesUrl.my} />
-    </div>;
+    return <Layout title='我的'>
+        <div className={styles.my}>
+            <img src={Logo} alt="" />
+            <span className={styles.title}>React H5 开发模板</span>
+            <span className={styles.github}>项目地址：
+                <a href="https://github.com/talktao/talk-react-app">https://github.com/talktao/talk-react-app</a>
+            </span>
+            <span className={styles.author}>项目作者：talktao</span>
+            <span className={styles.btn}>
+                {magicStore.magicText === '' ? <button onClick={onMagic}>点我有魔法</button> : magicStore.magicText}
+            </span>
+            <Tabbar path={routesUrl.my} />
+        </div>
+    </Layout>;
+
+
 };
 
 export default observer(My);

a2b3deedfa29c12d46ac4eef0a9f2514a08efc5c
tantao <t1416249906@163.com>
2023-05-16 21:00:36 +0800

    feat：react-router@6 ScrollRestoration 记住滚动的位置

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -4,21 +4,28 @@ import useListPages from '@/hooks/useListPages';
 import FetchTips from '@/components/fetchTips';
 import styles from './index.module.less';
 import Card from './card';
+import { ScrollRestoration } from 'react-router-dom';
+import Layout from '@/components/layout';
 
 const List: FC = () => {
 
-    const { list, ifDone } = useListPages(ApiCollector.getList, {});
+    const { loading, list, ifDone } = useListPages(ApiCollector.getList, {});
 
-    return <div className={styles.list}>
-        {
-            list.map((li, index) => (
-                <div className={styles.item} key={index}>
-                    <Card li={li} />
-                </div>
-            ))
-        }
-        {list.length !== 0 && <FetchTips ifDone={ifDone} />}
-    </div>;
+
+    return <Layout title='分页列表'>
+        <div className={styles.list}>
+            <ScrollRestoration getKey={(location, matches) => location.pathname} />
+            {
+                list?.map((li, index) => (
+                    <div className={styles.item} key={index}>
+                        <Card li={li} loading={loading} />
+                    </div>
+                ))
+            }
+            {list.length !== 0 && <FetchTips ifDone={ifDone} />}
+        </div>
+    </Layout>;
+
 };
 
 export default List;

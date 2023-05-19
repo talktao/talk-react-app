e59ff5f1d41261fb27320d20e88ea796f4e8ce25
tantao <t1416249906@163.com>
2023-05-19 23:02:02 +0800

    re：list coding check

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -2,26 +2,27 @@ import { FC } from 'react';
 import ApiCollector from '@/const/apis';
 import useListPages from '@/hooks/useListPages';
 import FetchTips from '@/components/fetchTips';
-import styles from './index.module.less';
 import Card from './card';
-import { ScrollRestoration } from 'react-router-dom';
 import Layout from '@/components/layout';
+import ListLoader from '@/components/skeleton/listLoader';
+import styles from './index.module.less';
 
 const List: FC = () => {
-
     const { loading, list, ifDone } = useListPages(ApiCollector.getList, {});
 
+    // 如果请求还在加载，则渲染骨架屏
+    if (loading) return <ListLoader />;
 
     return <Layout title='分页列表'>
         <div className={styles.list}>
-            <ScrollRestoration getKey={(location, matches) => location.pathname} />
             {
                 list?.map((li, index) => (
                     <div className={styles.item} key={index}>
-                        <Card li={li} loading={loading} />
+                        <Card li={li} />
                     </div>
                 ))
             }
+            {/* 底部加载时的请求提示 */}
             <FetchTips ifDone={ifDone} />
         </div>
     </Layout>;

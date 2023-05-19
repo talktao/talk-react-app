2da87705c9126ae3a7d645ccf3907febbf3bf03d
tantao <t1416249906@163.com>
2023-05-19 22:56:22 +0800

    fix：render coding check

--- src/helpers/render.ts
+++ src/helpers/render.ts
@@ -2,14 +2,10 @@ import ReactDOM from 'react-dom/client';
 
 export default async function render(App) {
 
-    // 获取语言环境
-    const lng = location.href.includes('zh-CN') ? 'zh_CN' : (location.href.includes('en') ? 'en' : 'zh_CN'); // 默认中文
-
     // 获取运行环境
     const UA = window.navigator.userAgent;
     const isMiniProgram = /miniprogram/i.test(UA);
     window['isMiniProgram'] = isMiniProgram ? true : false;
-    window['lng'] = lng;
 
     // 挂载 渲染应用
     const run = () => {
@@ -20,12 +16,11 @@ export default async function render(App) {
     };
 
     try {
-
         // try do something
     } catch (err) {
 
     } finally {
-        run()
+        run();
     }
 }
 

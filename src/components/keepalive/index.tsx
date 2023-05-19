d0c23a7dfc9853816dbe6654e22d9b3189df4e47
tantao <t1416249906@163.com>
2023-05-19 23:28:39 +0800

    fix：修复路径为‘/’，则重定向到home页

--- src/components/keepalive/index.tsx
+++ src/components/keepalive/index.tsx
@@ -1,14 +1,19 @@
+import { routesUrl } from "@/const/routes";
 import { useUpdate } from "ahooks";
 import { useEffect, useRef } from "react";
-import { useLocation, useOutlet } from "react-router-dom";
+import { useLocation, useNavigate, useOutlet } from "react-router-dom";
 
 function KeepAlive() {
     const componentList = useRef(new Map());
     const outLet = useOutlet();
+    const navigate = useNavigate();
     const { pathname } = useLocation();
     const forceUpdate = useUpdate();
 
     useEffect(() => {
+
+        // 如果路径时‘/’，则重定向到home页
+        if (pathname === '/') return navigate(routesUrl.home);
 
         if (!componentList.current.has(pathname)) {
             componentList.current.set(pathname, outLet);

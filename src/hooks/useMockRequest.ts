703eee72f8fddc98d9584aad8e1d71da86fd4a73
tantao <t1416249906@163.com>
2023-05-19 22:57:43 +0800

    feat：add Some comments

--- src/hooks/useMockRequest.ts
+++ src/hooks/useMockRequest.ts
@@ -4,6 +4,11 @@ import { useRequest } from "ahooks";
 import get from 'lodash/get';
 import useAxiosMethods from "./useAxiosMethods";
 
+/**
+ * @param request method:请求方式，url：请求路径
+ * @param params data: 接口请求参数，config：ahook的useRequest的第二个参数
+ * @returns 核心数据data和useRequest给我们提供的一系列数据及方法
+ */
 
 export default function useMockRequest<T>(request: RequestTuple, params: RequestProps<T>) {
     const { method, url } = request;
@@ -12,10 +17,18 @@ export default function useMockRequest<T>(request: RequestTuple, params: Request
 
     if (!controller[method]) throw new Error('当前请求方法仅支持get/post/put/delete');
 
+    // 发送请求的函数
     const http = () => controller[method](url, data);
+
+    // 通过useRequest发送请求
     const alibabaHook = useRequest(http, config);
+
+    // 拿到返回值
     const { data: res } = alibabaHook;
+
+    // 获取接口中的数据
     const returnData = get(res, 'data.data', []) as T;
+
     return {
         ...alibabaHook,
         data: returnData

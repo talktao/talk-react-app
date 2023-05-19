71fc5cc353b21f3f5310b1d46ed9bcd43a18ae41
tantao <t1416249906@163.com>
2023-05-19 22:54:43 +0800

    re：tababr coding check and change style

--- src/components/tabbar/index.tsx
+++ src/components/tabbar/index.tsx
@@ -3,21 +3,16 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { tabbarConfig } from './config';
 import style from './index.module.less';
 
-type tabbarType = {
-    path: string;
-};
-
-const Tabbar: FC<tabbarType> = () => {
-
+const Tabbar: FC = () => {
     const navigate = useNavigate();
-    const loacation = useLocation();
+    const { pathname } = useLocation();
 
     const tabbarList = useMemo(() => tabbarConfig.map(tab => (
         <div key={tab.name} className={style.tabbarItem} onClick={() => navigate(tab.route)}>
-            <img src={loacation.pathname === tab.route ? tab.active : tab.icon} alt="" />
-            <div className={loacation.pathname === tab.route ? style.active : ''}>{tab.name}</div>
+            <img src={pathname === tab.route ? tab.active : tab.icon} alt="" />
+            <div className={pathname === tab.route ? style.active : ''}>{tab.name}</div>
         </div>
-    )), [loacation.pathname]);
+    )), [pathname]);
 
     return <div className={style.tabbar}>
         {tabbarList}

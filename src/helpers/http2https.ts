58b73ce0766a7c3449eff60c8a086abf721f69a6
tantao <t1416249906@163.com>
2023-05-20 01:14:48 +0800

    fix: mock 数据的img地址默认是为http，现转为https

---
+++ src/helpers/http2https.ts
@@ -0,0 +1,3 @@
+export const http2https = (url: string) => {
+    return url.replace('http', 'https');
+};
--- src/pages/list/card/index.tsx
+++ src/pages/list/card/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import styles from './index.module.less';
+import { http2https } from "@/helpers/http2https";
 
 interface LiProps {
     id: number;
@@ -16,7 +17,7 @@ type CardType = {
 const Card: FC<CardType> = ({ li }) => {
 
     return <div className={styles.card}>
-        <img src={li.image} loading="lazy" alt="" />
+        <img src={http2https(li.image)} loading="lazy" alt="" />
         <div className={styles.info}>
             <p className={styles.name}>{li.name}</p>
             <p className={styles.descript}>{li.descript}</p>
